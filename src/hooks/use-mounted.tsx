1b415d81f482cafb062c74a04ae42d8ab6c46824
edufly21 <155825251+edufly21@users.noreply.github.com>
2024-01-13 23:52:13 +0300

    Update use-mounted.tsx

--- src/hooks/use-mounted.tsx
+++ src/hooks/use-mounted.tsx
@@ -1,1 +1,12 @@
+import react, { useState, useEffect } from "react";
 
+export default function useMounted(){
+   const [mounted, setMounted] = useState(false)
+
+   useEffect(() => {
+     setMounted(true)
+   }, [])
+
+  return mounted
+
+}
