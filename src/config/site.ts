992319c8b412246f0d28f52481df24713e3a4b02
edufly21 <155825251+edufly21@users.noreply.github.com>
2024-01-14 11:58:17 +0300

    Update site.ts

--- src/config/site.ts
+++ src/config/site.ts
@@ -4,7 +4,7 @@ export const siteConfig: SiteConfig = {
   name: "Edufly",
   description:
     "Discover EduGifts: your go-to cyber haven for gifts, photo albums, and electronics. Elevate your gifting game effortlessly.",
-  url: process.env.next_PUBLIC_SERVER_URL!,
-  ogImage: `${process.env.next_PUBLIC_SERVER_URL}/og-image.svg`,
+  url: process.env.NEXT_PUBLIC_SERVER_URL as string,
+  ogImage: `${process.env.NEXT_PUBLIC_SERVER_URL}/og-image.svg`,
   links: [],
 };
