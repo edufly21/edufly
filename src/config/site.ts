424f441fb7e22e96ca76fcc6fec21355efb86d12
edufly21 <155825251+edufly21@users.noreply.github.com>
2024-01-14 12:01:08 +0300

    Update site.ts

--- src/config/site.ts
+++ src/config/site.ts
@@ -5,6 +5,5 @@ export const siteConfig: SiteConfig = {
   description:
     "Discover EduGifts: your go-to cyber haven for gifts, photo albums, and electronics. Elevate your gifting game effortlessly.",
   url: process.env.NEXT_PUBLIC_SERVER_URL as string,
-  ogImage: `${process.env.NEXT_PUBLIC_SERVER_URL}/og-image.svg`,
   links: [],
 };
