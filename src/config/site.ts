bae30f676e9490341e00fc26a57062902c93d3e0
edufly21 <155825251+edufly21@users.noreply.github.com>
2024-01-14 21:07:26 +0300

    Update site.ts

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,7 +1,7 @@
 import { SiteConfig } from "../types";
 
 export const siteConfig: SiteConfig = {
-  name: "Cabdikhaaliq.com",
+  name: "Madoobe.com",
   description:
     "Discover EduGifts: your go-to cyber haven for gifts, photo albums, and electronics. Elevate your gifting game effortlessly.",
   url: process.env.NEXT_PUBLIC_SERVER_URL as string,
