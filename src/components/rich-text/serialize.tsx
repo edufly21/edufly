5e8a2da4ad9d6bc4e8d5958f8654cd9bcee90291
edufly21 <155825251+edufly21@users.noreply.github.com>
2024-01-14 14:14:11 +0300

    Update serialize.tsx

--- src/components/rich-text/serialize.tsx
+++ src/components/rich-text/serialize.tsx
@@ -2,7 +2,6 @@ import React, { Fragment } from "react";
 import escapeHTML from "escape-html";
 import { cn } from "@/lib/utils/shadcn-ui";
 
-// CUSTOM ARE NOT WORKING FOR NOW
 
 type Node = {
   type: string;
@@ -105,7 +104,7 @@ export const Serialize: SerializeFunction = ({ content, customRenderers }) => {
               <h2
                 key={i}
                 className={cn(
-                  "mt-10 scroll-m-20 border-b pb-1 text-2xl font-semibold tracking-tight first:mt-0"
+                  "mt-10 scroll-m-20 pb-1 text-2xl font-semibold tracking-tight"
                 )}
               >
                 <Serialize
@@ -170,7 +169,7 @@ export const Serialize: SerializeFunction = ({ content, customRenderers }) => {
                 />
               </h6>
             );
-          case "quote":
+          case "blockquote":
             return (
               <blockquote
                 key={i}
@@ -211,22 +210,23 @@ export const Serialize: SerializeFunction = ({ content, customRenderers }) => {
                 />
               </li>
             );
-          //
-        // case "link":
-           // return (
-              // <CMSLink
-                 //key={i}
-               // type={node.linkType === "internal" ? "reference" : "custom"}
-               // url={node.url}
-               //reference={node.doc as Reference}
-               //  newTab={node?.newTab}
-          //     >
-          //       <Serialize
-          //         content={node.children}
-          //         customRenderers={customRenderers}
-          //       />
-          //     </CMSLink>
-          //   );
+          
+        case "link":
+            return (
+               <a
+                key={i}
+                //type={node.linkType === "internal" ? "reference" : "custom"}
+                href={node.url}
+              // reference={node.doc as Reference}
+                 target={node?.newTab ? "_blank" : undefined}
+                 className={cn("font-medium underline underline-offset-4")}
+               >
+                 <Serialize
+                  content={node.children}
+                  customRenderers={customRenderers}
+                />
+               </a>
+             );
 
           // case "upload": {
           //   return <RichTextUpload key={i} node={node} />;
@@ -265,7 +265,7 @@ export const Serialize: SerializeFunction = ({ content, customRenderers }) => {
 
           default:
             return (
-              <p key={i} className={cn("leading-none text-red-500 [&:not(:first-child)]:mt-6")}>
+              <p key={i} className={cn("leading-7 mt-6")}>
                 <Serialize
                   content={node.children}
                   customRenderers={customRenderers}
