5c1840aa8d726d0c99a4083e8bf5a9c66dd14e89
edufly21 <155825251+edufly21@users.noreply.github.com>
2024-01-14 19:30:10 +0300

    Update page.tsx

--- src/app/(marketing)/blog/page.tsx
+++ src/app/(marketing)/blog/page.tsx
@@ -18,7 +18,7 @@ export default async function BlogPage() {
     <div className="container max-w-4xl py-6 lg:py-10">
       <div className="flex flex-col items-start gap-4 md:flex-row md:justify-between md:gap-8">
         <div className="flex-1 space-y-4">
-          <h1 className="inline-block font-heading text-4xl tracking-tight lg:text-5xl">
+          <h1 className="inline-block font-bold text-4xl tracking-tight lg:text-5xl">
             Blogs
           </h1>
           <p className="text-lg text-muted-foreground">
@@ -26,7 +26,7 @@ export default async function BlogPage() {
           </p>
         </div>
       </div>
-      <hr className="my-8 bg-muted-foreground text-muted-foreground" />
+      <hr className="my-8 bg-slate-300 bg-slate-800" />
       {blogs?.length ? (
         <div className="grid gap-10 sm:grid-cols-2">
           {blogs.map((blog, index) => (
