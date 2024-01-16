cc0e091233425f9add7a53af49a2e8e03d49efda
edufly21 <155825251+edufly21@users.noreply.github.com>
2024-01-16 22:58:41 +0300

    Update blog-reel.tsx

--- src/components/blog/blog-reel.tsx
+++ src/components/blog/blog-reel.tsx
@@ -65,17 +65,17 @@ const BlogsReel = (props: BlogReelProps) => {
   }
 
   return (
-    <section className="py-12">
+    <section>
       <div className="md:flex md:items-center md:justify-between mb-4">
         <div className="max-w-2xl px-4 lg:max-w-4xl lg:px-0">
-          {title ? (
+          {title && (
             <h1 className="text-2xl font-bold text-gray-900 sm:text-3xl">
               {title}
             </h1>
-          ) : null}
-          {subtitle ? (
+          ) }
+          {subtitle && (
             <p className="mt-2 text-sm text-muted-foreground">{subtitle}</p>
-          ) : null}
+          ) }
         </div>
 
         {link ? (
@@ -83,7 +83,7 @@ const BlogsReel = (props: BlogReelProps) => {
             href={link.href}
             className="hidden text-sm font-medium text-blue-600 hover:text-blue-500 md:block"
           >
-            Shop the collection <span aria-hidden="true">&rarr;</span>
+            {link.text} <span aria-hidden="true">&rarr;</span>
           </Link>
         ) : null}
       </div>
