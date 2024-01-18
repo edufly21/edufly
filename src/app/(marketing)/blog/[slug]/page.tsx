d424f8cbfbb9b06a4e892a3fe4f602a151337f2b
edufly21 <155825251+edufly21@users.noreply.github.com>
2024-01-18 23:27:33 +0300

    Update page.tsx

--- src/app/(marketing)/blog/[slug]/page.tsx
+++ src/app/(marketing)/blog/[slug]/page.tsx
@@ -1,4 +1,4 @@
-import RenderBlocks from "@/components/blocks/render-blocks";
+import RenderBlocks from "@/components/blocks/render-blocks.";
 import BlogsReel from "@/components/blog/blogs-reel";
 import { ChevronLeft } from "@/components/icons";
 import Moment from "@/components/shared/moment";
